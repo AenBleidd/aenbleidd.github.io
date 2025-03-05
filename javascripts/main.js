38a09abe11a415577c138b4fddfa0c11564ef2c6
Vitalii Koshura <lestat.de.lionkur@gmail.com>
2025-03-05 14:28:48 +0100

    Add 2024.02.28.md file
    Signed-off-by: Vitalii Koshura <lestat.de.lionkur@gmail.com>

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,10 +1,28 @@
 import { marked } from "https://cdn.jsdelivr.net/npm/marked/lib/marked.esm.js";
 console.log('This would be the main JS file.');
-console.log('AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA');
-fetch('blog/2023.10.14.md')
-  .then(response => response.text())
-  .then(text => {
-    const markdownContent = marked.parse(text);
-    document.getElementById("blog_content").innerHTML = markdownContent;
-  })
-  .catch(error => console.error('Error fetching the markdown file:', error));
+
+const markdownFiles = [
+  'blog/2024.02.28.md',
+  'blog/2023.10.14.md',
+];
+
+document.addEventListener('DOMContentLoaded', function() {
+  const footer = document.createElement('footer');
+  const p = document.createElement('p');
+  p.textContent = '© 2025 Vitalii Koshura';
+  footer.appendChild(p);
+  document.body.appendChild(footer);
+
+  const blogContent = document.getElementById('blog_content');
+  markdownFiles.forEach(file => {
+    fetch(file)
+      .then(response => response.text())
+      .then(text => {
+        const markdownContent = marked.parse(text);
+        const section = document.createElement('section');
+        section.innerHTML = markdownContent;
+        blogContent.appendChild(section);
+      })
+      .catch(error => console.error('Error fetching the markdown file:', error));
+  });
+});
