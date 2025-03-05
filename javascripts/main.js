17ea05e396cd13bc6dd0cc173e9db6d7e51b396b
Vitalii Koshura <lestat.de.lionkur@gmail.com>
2025-03-05 14:13:09 +0100

    Add 2023.10.14.md file
    Signed-off-by: Vitalii Koshura <lestat.de.lionkur@gmail.com>

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,9 +1,10 @@
+import { marked } from "https://cdn.jsdelivr.net/npm/marked/lib/marked.esm.js";
 console.log('This would be the main JS file.');
-
-document.addEventListener('DOMContentLoaded', function() {
-  const footer = document.createElement('footer');
-  const p = document.createElement('p');
-  p.textContent = '© 2023 Your Company. All rights reserved.';
-  footer.appendChild(p);
-  document.body.appendChild(footer);
-});
+console.log('AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA');
+fetch('blog/2023.10.14.md')
+  .then(response => response.text())
+  .then(text => {
+    const markdownContent = marked.parse(text);
+    document.getElementById("blog_content").innerHTML = markdownContent;
+  })
+  .catch(error => console.error('Error fetching the markdown file:', error));
