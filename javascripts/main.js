2503f0be048d6fd071cd20643c6770630db75a14
Vitalii Koshura <lestat.de.lionkur@gmail.com>
2025-03-05 13:32:03 +0100

    Clean up
    Signed-off-by: Vitalii Koshura <lestat.de.lionkur@gmail.com>

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,9 @@
 console.log('This would be the main JS file.');
+
+document.addEventListener('DOMContentLoaded', function() {
+  const footer = document.createElement('footer');
+  const p = document.createElement('p');
+  p.textContent = '© 2023 Your Company. All rights reserved.';
+  footer.appendChild(p);
+  document.body.appendChild(footer);
+});
