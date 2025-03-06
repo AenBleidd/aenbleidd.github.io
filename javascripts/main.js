4abbc9aaa61486d6cf4bc100913d3b6af2ca4bbe
Vitalii Koshura <lestat.de.lionkur@gmail.com>
2025-03-06 01:47:19 +0100

    Adding 2025.01.01 file. Refactoring
    Signed-off-by: Vitalii Koshura <lestat.de.lionkur@gmail.com>

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,14 +1,15 @@
 import { marked } from "https://cdn.jsdelivr.net/npm/marked/lib/marked.esm.js";
 
 const markdownFiles = [
-  { file: 'blog/2024.12.01.md', title: 'BOINC Development Status Report: November 2024' },
-  { file: 'blog/2024.11.02.md', title: 'BOINC Development Status Report: October 2024' },
-  { file: 'blog/2024.10.13.md', title: 'BOINC Workshop 2024: What\'s next?' },
-  { file: 'blog/2024.04.01.md', title: 'BOINC Release 8.0.0 and liblzma vulnerability' },
-  { file: 'blog/2024.03.18.md', title: 'Major BOINC version change' },
-  { file: 'blog/2024.03.16.md', title: 'Android BOINC: where are my GPUs?' },
-  { file: 'blog/2024.02.28.md', title: 'Vanilla BOINC packages: the reason and the purpose' },
-  { file: 'blog/2023.10.14.md', title: 'Introduction' }
+  { file: '2025.01.01.md', title: 'BOINC Development Status Report: December 2024' },
+  { file: '2024.12.01.md', title: 'BOINC Development Status Report: November 2024' },
+  { file: '2024.11.02.md', title: 'BOINC Development Status Report: October 2024' },
+  { file: '2024.10.13.md', title: 'BOINC Workshop 2024: What\'s next?' },
+  { file: '2024.04.01.md', title: 'BOINC Release 8.0.0 and liblzma vulnerability' },
+  { file: '2024.03.18.md', title: 'Major BOINC version change' },
+  { file: '2024.03.16.md', title: 'Android BOINC: where are my GPUs?' },
+  { file: '2024.02.28.md', title: 'Vanilla BOINC packages: the reason and the purpose' },
+  { file: '2023.10.14.md', title: 'Introduction' }
 ];
 
 document.addEventListener('DOMContentLoaded', function() {
@@ -25,14 +26,13 @@ document.addEventListener('DOMContentLoaded', function() {
     }
   });
 
-  const basePath = window.location.pathname.includes('/blog/') ? '../' : '';
 
   markdownFiles.forEach(({ file, title }) => {
     // Add menu item
     const li = document.createElement('li');
     const a = document.createElement('a');
     const htmlFile = file.replace('.md', '.html');
-    a.href = `${basePath}${htmlFile}`;
+    a.href = `${htmlFile}`;
     a.textContent = title;
     li.appendChild(a);
     blogItems.appendChild(li);
