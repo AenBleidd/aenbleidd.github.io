c0d445dc67cab25c0dbbeee55483d3e637e8a9f9
Vitalii Koshura <lestat.de.lionkur@gmail.com>
2025-07-20 19:11:05 +0200

    Try to fix sitemap.xml file
    Signed-off-by: Vitalii Koshura <lestat.de.lionkur@gmail.com>

--- deploy.py
+++ deploy.py
@@ -175,7 +175,7 @@ with open('rss.xml', 'w', encoding='utf-8') as f:
     f.write('</feed>')
 
 with open('sitemap.xml', 'w', encoding='utf-8') as f:
-    f.write("""<?xml version='1.0' encoding='UTF-8'?><urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">""")
+    f.write("""<?xml version="1.0" encoding="UTF-8"?><urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">""")
     f.write(f'<url><loc>https://aenbleidd.github.io/</loc><lastmod>{blog_posts[-1]["file"].replace(".", "-")}T00:00:00Z</lastmod></url>')
     f.write(f'<url><loc>https://aenbleidd.github.io/blog.html</loc><lastmod>{blog_posts[-1]["file"].replace(".", "-")}T00:00:00Z</lastmod></url>')
     for post in reversed(blog_posts):
